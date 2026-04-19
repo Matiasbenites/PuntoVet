8171db35836c63a5c395610db428cbf37fcac392
matias <matiasbenites665@gmail.com>
2026-04-19 17:11:24 -0300

    Chore: Refinamiento de sintaxis

--- Backend/REST-API/app.js
+++ Backend/REST-API/app.js
@@ -4,10 +4,12 @@
 require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
+const { db } = require('./config/database');
 
 
 const app = express();
-const port = process.env.PORT || 3000;
+db();
+const port = process.env.PORT || 4000;
 
 
 app.use(cors());
@@ -17,7 +19,7 @@ app.use(express.static('storage'));
 app.use('/', require('./routes'));
 
 app.listen(port, () => {
-    console.log(`API_REST corriendo enel puerto:  ${port}`);
+    console.log(`API_REST corriendo en el puerto:  ${port}`);
 });
 
 
